90ead5d39a864a0d46a64b86e12674dff5478af2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,22 @@
-output "spring_cloud_configuration_services" {
-  description = "All spring_cloud_configuration_service resources"
-  value       = azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services
-  sensitive   = true
-}
 output "spring_cloud_configuration_services_generation" {
-  description = "List of generation values across all spring_cloud_configuration_services"
-  value       = [for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : v.generation]
+  description = "Map of generation values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.generation }
 }
 output "spring_cloud_configuration_services_name" {
-  description = "List of name values across all spring_cloud_configuration_services"
-  value       = [for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : v.name]
+  description = "Map of name values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.name }
 }
 output "spring_cloud_configuration_services_refresh_interval_in_seconds" {
-  description = "List of refresh_interval_in_seconds values across all spring_cloud_configuration_services"
-  value       = [for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : v.refresh_interval_in_seconds]
+  description = "Map of refresh_interval_in_seconds values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.refresh_interval_in_seconds }
 }
 output "spring_cloud_configuration_services_repository" {
-  description = "List of repository values across all spring_cloud_configuration_services"
-  value       = [for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : v.repository]
+  description = "Map of repository values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.repository }
   sensitive   = true
 }
 output "spring_cloud_configuration_services_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_configuration_services"
-  value       = [for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.spring_cloud_service_id }
 }
 
