751234d3f3cdc46b27e66c8af0ea49911b7668e3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_configuration_services_id" {
+  description = "Map of id values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
+  value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.id }
+}
 output "spring_cloud_configuration_services_generation" {
   description = "Map of generation values across all spring_cloud_configuration_services, keyed the same as var.spring_cloud_configuration_services"
   value       = { for k, v in azurerm_spring_cloud_configuration_service.spring_cloud_configuration_services : k => v.generation }
